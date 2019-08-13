51f191ae0602c1e7536ccfd2df299a7f65e42702
Gabriel Nagy <gabriel.nagy@puppet.com>
2019-08-13 13:32:34 +0300

    (MODULES-9578) Add tests for custom `target`
    Update the create/destroy/modify acceptance tests to cover cases when
    the optional `target` parameter is set.

--- spec/acceptance/tests/resource/ssh_authorized_key/create_spec.rb
+++ spec/acceptance/tests/resource/ssh_authorized_key/create_spec.rb
@@ -7,13 +7,11 @@ RSpec.context 'ssh_authorized_key: Create' do
   let(:name) { "pl#{rand(999_999).to_i}" }
   let(:custom_key_directory) { "/etc/ssh_authorized_keys_#{name}" }
   let(:custom_key) { "#{custom_key_directory}/authorized_keys_#{name}" }
-  let(:custom_name) { "custom_#{name}" }
 
   before(:each) do
     posix_agents.each do |agent|
-      on(agent, "cp #{auth_keys} /tmp/auth_keys", acceptable_exit_codes: [0, 1])
+      on(agent, "cp -a #{auth_keys} /tmp/auth_keys", acceptable_exit_codes: [0, 1])
       on(agent, "rm -f #{auth_keys}")
-      on(agent, "mkdir #{custom_key_directory}")
     end
   end
 
@@ -21,7 +19,6 @@ RSpec.context 'ssh_authorized_key: Create' do
     posix_agents.each do |agent|
       # (teardown) restore the #{auth_keys} file
       on(agent, "mv /tmp/auth_keys #{auth_keys}", acceptable_exit_codes: [0, 1])
-      on(agent, "rm -rf #{custom_key_directory}")
     end
   end
 
@@ -39,16 +36,28 @@ RSpec.context 'ssh_authorized_key: Create' do
     end
 
     it "#{agent} should create an entry for an SSH authorized key in a custom location" do
-      custom_args = ['ensure=present',
-                     'user=$LOGNAME',
-                     "type='rsa'",
-                     "key='mykey'",
-                     "target='#{custom_key}'"]
-
-      on(agent, puppet_resource('ssh_authorized_key', custom_name.to_s, custom_args))
+      on(agent, "mkdir #{custom_key_directory}")
+      args = ['ensure=present',
+              'user=$LOGNAME',
+              "type='rsa'",
+              "key='mykey'",
+              "target='#{custom_key}'"]
+      on(agent, puppet_resource('ssh_authorized_key', name.to_s, args))
 
       on(agent, "cat #{custom_key}") do |_res|
-        fail_test "didn't find the ssh_authorized_key for #{custom_name}" unless stdout.include? name.to_s
+        fail_test "didn't find the ssh_authorized_key for #{name}" unless stdout.include? name.to_s
+      end
+      on(agent, "rm -rf #{custom_key_directory}")
+    end
+
+    it "#{agent} should not create directories for SSH authorized key in a custom location" do
+      args = ['ensure=present',
+              'user=$LOGNAME',
+              "type='rsa'",
+              "key='mykey'",
+              "target='#{custom_key}'"]
+      on(agent, puppet_resource('ssh_authorized_key', name.to_s, args), acceptable_exit_codes: [0, 1]) do |_res|
+        fail_test unless stderr =~ %r{No such file or directory}
       end
     end
   end
